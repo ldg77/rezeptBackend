6ecdca2526afada957f4a6769b1067b2c9ac1d0b
Dumitru Gurduza <dimulya@gmail.com>
2022-12-08 16:30:08 +0100

    sadfsdf

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,15 +1,19 @@
-import Comment from "../models/CommentModel.js";
 import Recipe from "../models/RecipeModel.js";
-const saveComment  =async (req,res) =>{
-        try {
-               const filter ={_id:(req.params.id)} 
-               const recipe = await Recipe.findById(filter)
-               const updatedRecipe = await Recipe.findByIdAndUpdate(filter,{comments:[...recipe.comments,req.body]},{new:true});
-               console.log(updatedRecipe);
-               res.status(201).json(updatedRecipe)
-        } catch (error) {
-                console.log(error);
-        }
-}
+import Comment from "../models/CommentModel.js";
+const saveComment = async (req, res) => {
+  try {
+    const comment = await Comment.create(req.body);
+    const recipe = await Recipe.findById(req.params.id);
+    const updatedRecipe = await Recipe.findByIdAndUpdate(
+      recipe._id,
+      { comments: [...recipe.comments, comment._id] },
+      { new: true }
+    );
+    console.log(updatedRecipe);
+    res.status(201).json(updatedRecipe);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-export {saveComment}
+export { saveComment };
